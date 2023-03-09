5e7a3205e935bb145f42bf4387524ff0e551c665
tomQuintana <tomQuintana@gmail.com>
2023-03-08 21:39:51 -0300

    change eslinta airb for normal eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-multiple-empty-lines': [2, {'max': 99999, 'maxEOF': 0}],
+    'no-unused-vars': 'off'
+  }
+}
