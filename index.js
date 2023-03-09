4bbbb3e722819bbfb5a16a753c42d83ff7d30365
tomQuintana <tomQuintana@gmail.com>
2023-03-09 00:48:28 -0300

    add a routes with a comunication how to the controller

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import conectarDB from './config/db.js'
+import routerFood from './routes/food.routes.js'
 
 const app = express()
+
+app.use(express.json())
 
 dotenv.config()
 
 conectarDB()
+
+app.use('/api/food', routerFood)
 
 app.listen(4000, () => {
   console.log('Servidor corriendo en el puerto 4000')
