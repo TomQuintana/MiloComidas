cc8c97ed1d6eab1e8e2f9b18a559e47de567fa18
tomQuintana <tomQuintana@gmail.com>
2023-03-11 18:57:39 -0300

    set a registerFood controller

--- controllers/food.controller.js
+++ controllers/food.controller.js
@@ -1,9 +1,21 @@
+import validateType from '../helpers/validateType.js'
+import {generateSaveFood} from '../helpers/dbActions.js'
 
-const registerFood = (req, res) => {
-  console.log('Desde registerFood')
-  return res.json({
-    msg: 'Desde register food'
-  })
+const defaultTypes = ['fruta','verdura','desayuno','pollo','pescado','otros']
+const registerFood = async (req, res) => {
+
+  const {type} = req.body
+  // validateType(type, defaultTypes true si hace match
+  const trueType = validateType(type, defaultTypes)
+
+  if(!trueType) {
+    res.status(404).json({mag: 'The entered type is no valid, please enter a valid type'})
+    return
+  }
+
+  const mealRes = await generateSaveFood(req.body)
+
+  return res.json({mealRes})
 }
 
 export {
